0979155dd9502e71533fab9307f5508327e9171f
lmurayam <lmurayam@ucsc.edu>
2023-10-11 18:13:56 -0700

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 let config = {
-    type: Phaser.CANVAS,
+    type: Phaser.AUTO,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [Menu, Play]
 }
 
 let game = new Phaser.Game(config);
+
+// UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+
---
+++ src/scenes/Play.js
@@ -0,0 +1,29 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    preload() {
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    create(){
+        console.log("play")
+
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+    }
+
+    update() {
+        console.log(this.starfield.tilePositionX)
+        
+        this.starfield.tilePositionX -= 4;
+    }
+}
